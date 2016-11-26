1a7f5b577740c89d8a15412b39ff890a172c5369
3846masa <3846masahiro+git@gmail.com>
2016-11-26 19:54:23 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  settings: {
+    footnotes: true,
+  },
+  plugins: {
+    '@paperist/remark-mathjax': null,
+    '@paperist/remark-crossref': null,
+    '@paperist/remark-table-caption': null,
+    '@paperist/remark-latex': null,
+  },
+};
